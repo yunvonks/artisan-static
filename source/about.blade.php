337a9a1b53535e8ca4c7d3956be27ea469b6f0b1
Fakhruriyun <90853571+yunvonks@users.noreply.github.com>
2021-12-22 17:22:41 +0700

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -5,12 +5,12 @@
 @section('content')
     <h1>About</h1>
 
-    <p>My name is {{ $page->owner->name }}</p>
+    <p>My Name Is {{ $page->owner->name }}</p>
 
     <h2>Links:</h2>
 
     <ul>
-        <li><a href="/twitter" target="_blank">Twitter</a></li>
-        <li><a href="/github" target="_blank">GitHub</a></li>
+        <li><a href="https://fakhrurpedia.com" target="_blank">Fakhrurpedia</a></li>
+        <li><a href="https://github.com/yunvonks" target="_blank">GitHub</a></li>
     </ul>
 @endsection
