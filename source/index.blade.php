485b562fc79685f3af7aad9a0cca36b88a6f4914
Fakhruriyun <90853571+yunvonks@users.noreply.github.com>
2021-12-22 17:29:47 +0700

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -9,13 +9,13 @@
 
     <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
 
-    <a href="https://ko-fi.com/Y8Y07ACJ" target="_blank" rel="noopener">
-        <img src="{{ media('kofi-button.png') }}" alt="ko-fi" width="223" height="30">
+    <a href="https://fakhrurpedia.com" target="_blank" rel="noopener">
+        <img src="{{ media('fakhrurpedia.png') }}" alt="ko-fi" width="223" height="30">
     </a>
 
     <h2 id="test">Asset Compilation Test</h2>
 
     <p>CSS test: <span class="test-css">text with blue border</span></p>
 
-    <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
+    <p>Hompage: <a href="https://fakhrurpedia.com" class="test-js">click me</a></p>
 @endsection
