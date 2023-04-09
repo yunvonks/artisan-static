fb81f44863d39129eb46783752abb4bd78d0fe4c
Fakhrurpedia <90853571+yunvonks@users.noreply.github.com>
2023-04-09 22:41:58 +0700

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Artisan Static</h1>
+    <h1>TechnologyEsclair</h1>
 
     <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
 
