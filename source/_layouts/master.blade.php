c0c8ace3107044031bb0a89c2179e2373913cb50
Fakhrurpedia <90853571+yunvonks@users.noreply.github.com>
2023-04-09 22:55:47 +0700

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -41,7 +41,7 @@
                 &copy; <span data-year></span> {{ $page->owner->name }} &nbsp;&bull;&nbsp;
                 <a href="/feed.atom">RSS</a> &nbsp;&bull;&nbsp;
                 Maintained by Fakhrurpedia &nbsp;&bull;&nbsp;
-                <a href="https://github.com/raniesantos/artisan-static">GitHub repo</a>
+                <a href="https://github.com/yunvonks">GitHub Repository</a>
             </small>
         </footer>
     </section>
